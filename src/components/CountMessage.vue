51350e0c81121e9b6114296550ef23844114a1dd
murakami <murakami@kokochie.jp>
2024-07-16 10:02:01 +0900

    コンポーネント読み込みでエラー？

---
+++ src/components/CountMessage.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<p>10以上になった: {{ count }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CountMessage',
+	props: {
+		count: {
+			type: Number,
+			required: true,
+		},
+	},
+};
+</script>
--- src/views/CountView.vue
+++ src/views/CountView.vue
@@ -1,15 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import CountMessage from '../components/CountMessage.vue';
+
+const count = ref<number>(0);
+
+// カウントアップ
+const increment = () => {
+	count.value++;
+};
+
+// カウントダウン
+const decrement = () => {
+	count.value--;
+};
+</script>
+
 <template>
 	<main>
 		<h1>カウントテスト</h1>
+		<div>
+			<p>現在のカウント: {{ count }}</p>
+			<button @click="increment">カウントアップ</button>
+			<button @click="decrement">カウントダウン</button>
+			<CountMesssage v-if="count >= 10" :count="count" />
+		</div>
 	</main>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-	.about {
-		min-height: 100vh;
-		display: flex;
-		align-items: center;
-	}
-}
-</style>
